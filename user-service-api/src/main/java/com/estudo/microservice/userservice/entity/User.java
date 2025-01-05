7f5233216f464130072af3681373127d90b679e6
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-01-05 18:41:11 -0300

    feat: add user entity

---
+++ user-service-api/src/main/java/com/estudo/microservice/userservice/entity/User.java
@@ -0,0 +1,98 @@
+package com.estudo.microservice.userservice.entity;
+
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profiles;
+
+    public User() {}
+
+    public User(String id, String name, String email, String password, Set<ProfileEnum> profiles) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.profiles = profiles;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<ProfileEnum> getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(Set<ProfileEnum> profiles) {
+        this.profiles = profiles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", profiles=" + profiles +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(profiles, user.profiles);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(email);
+        result = 31 * result + Objects.hashCode(password);
+        result = 31 * result + Objects.hashCode(profiles);
+        return result;
+    }
+}
