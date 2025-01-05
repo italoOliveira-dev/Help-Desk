9bc18394b85b40c45fc3704d7561d11840ececa6
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-01-05 17:18:44 -0300

    chore: start user-service-api

---
+++ user-service-api/src/main/java/com/estudo/microservice/userservice/UserServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.microservice.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApiApplication.class, args);
+	}
+
+}
