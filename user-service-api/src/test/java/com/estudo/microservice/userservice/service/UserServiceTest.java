9a72eb8beb6f08a7f44f20ed6bd8c4a143949c30
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-01-09 00:22:01 -0300

    test: add test to find by id with success

---
+++ user-service-api/src/test/java/com/estudo/microservice/userservice/service/UserServiceTest.java
@@ -0,0 +1,44 @@
+package com.estudo.microservice.userservice.service;
+
+import com.estudo.microservice.userservice.entity.User;
+import com.estudo.microservice.userservice.mapper.UserMapper;
+import com.estudo.microservice.userservice.repository.UserRepository;
+import models.responses.UserResponse;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void findByIdShouldReturnUserResponseWhenIdExists() {
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(new User()));
+        Mockito.when(userMapper.fromEntity(Mockito.any(User.class))).thenReturn(Mockito.mock(UserResponse.class));
+
+        final var response = userService.findById("1");
+        assertNotNull(response);
+        assertEquals(UserResponse.class, response.getClass());
+        Mockito.verify(userRepository, Mockito.times(1)).findById("1");
+        Mockito.verify(userMapper, Mockito.times(1)).fromEntity(Mockito.any(User.class));
+    }
+}
