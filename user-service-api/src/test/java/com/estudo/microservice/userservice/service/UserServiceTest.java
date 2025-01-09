c4a754cac8b759837037a2dc07d7957e99573115
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-01-09 09:26:04 -0300

    test: add test to find by id with invalid id user service

--- user-service-api/src/test/java/com/estudo/microservice/userservice/service/UserServiceTest.java
+++ user-service-api/src/test/java/com/estudo/microservice/userservice/service/UserServiceTest.java
@@ -3,19 +3,21 @@ package com.estudo.microservice.userservice.service;
 import com.estudo.microservice.userservice.entity.User;
 import com.estudo.microservice.userservice.mapper.UserMapper;
 import com.estudo.microservice.userservice.repository.UserRepository;
+import models.exceptions.ResourceNotFoundException;
 import models.responses.UserResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class UserServiceTest {
 
     @InjectMocks
@@ -40,5 +42,14 @@ class UserServiceTest {
         assertEquals(UserResponse.class, response.getClass());
         Mockito.verify(userRepository, Mockito.times(1)).findById("1");
         Mockito.verify(userMapper, Mockito.times(1)).fromEntity(Mockito.any(User.class));
+    }
+
+    @Test
+    void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> userService.findById("1"));
+        Mockito.verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
+        Mockito.verify(userMapper, Mockito.times(0)).fromEntity(Mockito.any(User.class));
     }
 }
