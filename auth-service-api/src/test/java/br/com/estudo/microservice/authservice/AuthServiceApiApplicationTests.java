71efd06940a1a4a40f64b21c7b861761135e6c43
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-01-15 11:35:27 -0300

    chore: starter auth-service-api

---
+++ auth-service-api/src/test/java/br/com/estudo/microservice/authservice/AuthServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.microservice.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
