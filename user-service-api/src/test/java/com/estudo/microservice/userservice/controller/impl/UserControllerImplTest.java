913f576bca7f10e1e0d0413bcc85af1ab7fd9cae
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-01-14 23:59:22 -0300

    test: add integration test to save user controller with bad request, conflict and success

--- user-service-api/src/test/java/com/estudo/microservice/userservice/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/com/estudo/microservice/userservice/controller/impl/UserControllerImplTest.java
@@ -1,12 +1,16 @@
 package com.estudo.microservice.userservice.controller.impl;
 
 import com.estudo.microservice.userservice.entity.User;
+import com.estudo.microservice.userservice.mapper.UserMapper;
 import com.estudo.microservice.userservice.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.requests.CreateUserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,11 +26,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 class UserControllerImplTest {
 
+    public static final String BASE_URI = "/api/users";
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void findByIdShouldReturnIsOk() throws Exception {
@@ -63,12 +75,69 @@ class UserControllerImplTest {
 
         userRepository.saveAll(List.of(entity1, entity2));
 
-        mockMvc.perform(get("/api/users"))
+        mockMvc.perform(get(BASE_URI))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$.[0]").isNotEmpty())
                 .andExpect(jsonPath("$.[1]").isNotEmpty());
 
         userRepository.deleteAll(List.of(entity1, entity2));
+    }
+
+    @Test
+    void saveShouldReturnCreated() throws Exception {
+        final var validEmail = "validemail@email.com";
+        final var request = generateMock(CreateUserRequest.class).withEmail(validEmail);
+        final var json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                post(BASE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated());
+
+        userRepository.findByEmail(validEmail).ifPresent(user -> userRepository.delete(user));
+    }
+
+    @Test
+    void saveShouldReturnBadRequest() throws Exception {
+        final var request = generateMock(CreateUserRequest.class).withEmail("newEmail.com");
+        final var json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                post(BASE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.error").value(HttpStatus.BAD_REQUEST.getReasonPhrase()))
+                .andExpect(jsonPath("$.message").value("Exception in validation attributes"))
+                .andExpect(jsonPath("$.path").value(BASE_URI))
+                .andExpect(jsonPath("$.errors[?(@.fieldName=='email' && @.message=='Invalid email')]").exists());
+
+    }
+
+    @Test
+    void saveShouldReturnConflictWhenEmailAlreadyExists() throws Exception {
+        final var existsEmail = "exists@email.com";
+        final var requestExists = generateMock(CreateUserRequest.class).withEmail(existsEmail);
+        User user = userRepository.save(userMapper.fromRequest(requestExists));
+
+        final var newRequest = generateMock(CreateUserRequest.class).withEmail(existsEmail);
+        final var json = objectMapper.writeValueAsString(newRequest);
+
+        mockMvc.perform(
+                post(BASE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
+                .andExpect(jsonPath("$.error").value(HttpStatus.CONFLICT.getReasonPhrase()))
+                .andExpect(jsonPath("$.message").value(String.format("Email [ %s ] already exists!", existsEmail)))
+                .andExpect(jsonPath("$.path").value(BASE_URI));
+
+        userRepository.delete(user);
     }
 }
