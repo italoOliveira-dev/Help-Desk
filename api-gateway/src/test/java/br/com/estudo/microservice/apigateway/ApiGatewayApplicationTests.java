e0b987f474ec439821cedea546212138cd473ff6
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-04-15 12:50:35 -0300

    feat: run api-gateway on docker-compose

--- api-gateway/src/test/java/br/com/estudo/microservice/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/br/com/estudo/microservice/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.estudo.microservice.apigateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
